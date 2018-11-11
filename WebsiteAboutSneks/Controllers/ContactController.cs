0d401c252761e7e28208644b3e31d91429f60f89
mjsmith3 <44041226+mjsmith3@users.noreply.github.com>
2018-11-10 21:30:24 -0700

    branch

--- WebsiteAboutSneks/Controllers/ContactController.cs
+++ WebsiteAboutSneks/Controllers/ContactController.cs
@@ -6,15 +6,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteAboutSneks.Models;
 
 namespace WebsiteAboutSneks.Controllers
 {
     public class ContactController : Controller
     {
         // GET: Contact
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(ContactInfo contact)
+        {
+            /*This code is optional, but it would be for sending a confirmation email to whoever submitted a contact info form.
+            string sSubject="";
+            
+            if (contact.Subject==1)
+            {
+                sSubject = "Questions about snakes";
+            }
+            else if (contact.Subject==2)
+            {
+                sSubject = "My snake story";
+            }
+            else if (contact.Subject==3)
+            {
+                sSubject = "Snake Care";
+            }
+            else
+            {
+                sSubject = "Other";
+            }
+
+            ContactInfo.emailToAddress = contact.Email;
+            ContactInfo.subject = "Thank you for contacing us!";
+            ContactInfo.body = "Thank you for reaching out to us " + contact.Name + "! Our team is in the process of reviewing your message" +
+                ". We will get back to you as soon as we can." + "<br /><br />" + "Subject: " + sSubject + "<br /><br />" + "Message: " + contact.Message;
+            ContactInfo.SendEmail();*/
+            if (ModelState.IsValid)
+            {
+                return View("Confirmation", contact);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
     }
 }
