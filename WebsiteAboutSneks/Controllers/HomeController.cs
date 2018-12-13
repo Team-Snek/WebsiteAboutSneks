b4614e9e97941916fdc292659edb40d19752ba62
chelsialiu <43692077+chelsialiu@users.noreply.github.com>
2018-12-12 17:04:58 -0700

    Added using DAL back into Home Controller

--- WebsiteAboutSneks/Controllers/HomeController.cs
+++ WebsiteAboutSneks/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using WebsiteAboutSneks.DAL;
 using WebsiteAboutSneks.Models;
 
 namespace WebsiteAboutSneks.Controllers
